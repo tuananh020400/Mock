23794e824b016196e4fba6b20a9b2009aa864e46
Tuan Anh <tuananh020400@gmail.com>
2022-06-23 01:37:02 +0700

    Second

--- main.c
+++ main.c
@@ -6,36 +6,34 @@ extern FILE *file;
 
 void Print(Entry entry)
 {
-    uint8_t count = 0;
     static uint8_t no = 0;
+    
     if(entry.name[0] != 0)
     {
         no++;
         printf("%-4d", no);
-        for(int i = 0; i < 8; i++)
+        if(strcmp(entry.type, "TXT") == 0)
         {
-            printf("%c", entry.name[i]);
-            count++;
-        }
-        if(strcmp(entry.type,"   ") != 0)
-        {
+            for(int i = 0; i < 8; i++)
+            {
+                printf("%c", entry.name[i]);
+            }
             printf(".");
-            count = 0;
             for(int i = 0; i < 3; i++)
             {
                 printf("%c", entry.type[i]);
-                count++;
             }
             printf("   %-7d", entry.size);
-        }
-        if(strcmp(entry.type, "TXT") == 0)
-        {
             printf("%-10s", "Text Document");
             printf("%5s%02d/%02d/%-4d", "", entry.day, entry.month, entry.year + 1980);
             printf("%7s%02d:%02d:%02d", "", entry.hour, entry.minute, entry.second * 2);
         }
         else
         {
+            for(int i = 0; i < 8; i++)
+            {
+                printf("%c", entry.name[i]);
+            }
             printf("%-14s%-10s","","File Folder");
             printf("%7s%02d/%02d/%-4d", "", entry.day, entry.month, entry.year + 1980);
             printf("%7s%02d:%02d:%02d", "", entry.hour, entry.minute, entry.second * 2);
@@ -47,10 +45,10 @@ void Print(Entry entry)
 void ReadEntryRDET(uint8_t startSector, uint8_t *buffer)
 {
     uint32_t count = 0x00;
-    //uint16_t i = 0;
+    uint8_t i = 0;
     uint8_t nameCount = 0;
     uint8_t typeCount = 0;
-    Entry entryArray[];
+    Entry entryArray[16];
     
     printf("%-4s%-15s%-7s%-18s%-17s%-10s\n", "No", "Name", "Size", "Type", "Day Modified", "Time Modified");
     printf("---------------------------------------------------------------------------------------\n");
@@ -61,22 +59,22 @@ void ReadEntryRDET(uint8_t startSector, uint8_t *buffer)
         {
             for (nameCount = 0; nameCount < 8; nameCount++)
             {
-                entry.name[nameCount] = buffer[count + 0x00 + nameCount];
+                entryArray[i].name[nameCount] = buffer[count + 0x00 + nameCount];
             }
             for (typeCount= 0; typeCount < 3; typeCount++)
             {
-                entry.type[typeCount] = buffer[count + 0x08 + typeCount];
+                entryArray[i].type[typeCount] = buffer[count + 0x08 + typeCount];
             }
-            entry.size = ((uint32_t)buffer[count + 0x1C]) | ((uint32_t)buffer[count + 1 + 0x1C] << 4) | \
+            entryArray[i].size = ((uint32_t)buffer[count + 0x1C]) | ((uint32_t)buffer[count + 1 + 0x1C] << 4) | \
             ((uint32_t)buffer[count + 2 + 0x1C] << 8) | ((uint32_t)buffer[count + 3 + 0x1C] << 12); 
-            entry.day = buffer[count + 0x18] & (~0b11100000);
-            entry.month = ((buffer[count + 0x18] & (~0b00011111)) >> 5) | ((buffer[count + 0x18 + 0x01] & (~0b11111110)) << 3);
-            entry.year = ((buffer[count + 0x18 + 0x01] & (~0b00000001)) >> 1);
-            entry.second = buffer[count + 0x16] & (~0b11100000);
-            entry.minute = ((buffer[count + 0x16] & (~0b00011111)) >> 5) | ((buffer[count + 0x16 + 0x01] & (~0b11111000)) << 3);
-            entry.hour = (buffer[count + 0x16 + 0x01] & (~0b00000111)) >> 3;
+            entryArray[i].day = buffer[count + 0x18] & (~0b11100000);
+            entryArray[i].month = ((buffer[count + 0x18] & (~0b00011111)) >> 5) | ((buffer[count + 0x18 + 0x01] & (~0b11111110)) << 3);
+            entryArray[i].year = ((buffer[count + 0x18 + 0x01] & (~0b00000001)) >> 1);
+            entryArray[i].second = buffer[count + 0x16] & (~0b11100000);
+            entryArray[i].minute = ((buffer[count + 0x16] & (~0b00011111)) >> 5) | ((buffer[count + 0x16 + 0x01] & (~0b11111000)) << 3);
+            entryArray[i].hour = (buffer[count + 0x16 + 0x01] & (~0b00000111)) >> 3;
 
-            Print(entry);
+            Print(entryArray[i]);
         }
         // if((buffer[count + 0x0B] == 0x00))
         // {
